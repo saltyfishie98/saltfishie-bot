6990f28202529da5254ef9c8be8cb8b78aa62ab4
saltyfishie98 <randommaker2142@gmail.com>
2021-07-14 18:54:40 +0800

    test http request

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+const Express = require('express');
 const Discord = require('discord.js');
 const { prefix } = require('./config.json');
+
+const app = Express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+	res.send("hello world");
+});
+
+const listener = app.listen(port, () => {
+	console.log("Your app is listening on port " + listener.address().port);
+});
 
 const Client = new Discord.Client();
 Client.on('ready', () => {
