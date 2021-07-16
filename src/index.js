904808fb9e9d3f1204533711cb4b953a8192a31c
saltyfishie98 <randommaker2142@gmail.com>
2021-07-16 16:18:59 +0800

    change embed text

--- src/index.js
+++ src/index.js
@@ -47,12 +47,25 @@ client.on('message', message => {
 const Events = require('events');
 const testAnnounce = new Events();
 testAnnounce.on('stream-live', () => {
-	const exampleEmbed =
-		new Discord.MessageEmbed()
-			.setTitle('Live on twitch')
-			.setURL('https://www.twitch.tv/');
-
 	const annouceChannel = client.channels.cache.get("863447077094031393");
-	annouceChannel.send(exampleEmbed);
+	annouceChannel.send({
+		embed: {
+			color: 3447003,
+			author: {
+				name: client.user.username,
+				icon_url: client.user.displayAvatarURL()
+			},
+			title: "Stream going live",
+			thumbnail: {
+				url: "https://static-cdn.jtvnw.net/jtv_user_pictures/4430880f-aff9-4188-88e8-e52f7f67e81f-profile_image-70x70.png"
+			},
+			description: "Check it out [here](https://www.twitch.tv/benangz)",
+			timestamp: new Date(),
+			footer: {
+				icon_url: client.user.displayAvatarURL(),
+				text: "© Example"
+			}
+		}
+	});
 	console.log('testAnnouce: '.yellow + 'Announced\n');
 });
