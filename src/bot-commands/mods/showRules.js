4cec031d5958877d1cfd8c1884ecca1925c7caaa
saltyfishie98 <randommaker2142@gmail.com>
2021-07-21 20:42:57 +0800

    fixed and renamed rules to showRules

--- src/bot-commands/mods/showRules.js
+++ src/bot-commands/mods/showRules.js
@@ -6,11 +6,10 @@ module.exports = {
 	permissions: ["ADMINISTRATOR"],
 	requiredRoles: ["Admin"],
 	callback: async (message, args, text) =>{
-
-		let fetched = await message.channel.messages.fetch({ limit: 5 });
-		message.channel.bulkDelete(fetched);
-
 		if(message.channel.id === "866630865583603712"){
+			let fetched = await message.channel.messages.fetch({ limit: 5 });
+			message.channel.bulkDelete(fetched);	
+
 			const embedMessage = {
 				color: 3447003,
 				title: "By reacting to this message, you have agreed to the rules of this server"
@@ -19,6 +18,8 @@ module.exports = {
 			message.channel.send({
 				embed: embedMessage
 			});
+		} else {
+			message.channel.reply("can't be used in this channel");
 		}
 	}
 };
