a75b67f3e622190e06ebfaf01bbec6a1c6f86d5a
saltyfishie98 <randommaker2142@gmail.com>
2021-07-28 07:22:42 +0800

    turn off autoreact in dev

--- src/helper/suggestionAutoReact.js
+++ src/helper/suggestionAutoReact.js
@@ -1,20 +1,17 @@
 async function suggestionAutoReact(client){
 	const channelList = [
 		"868907390752399370",
-		"845682082750922788",
-		"865609005136216075"
+		"845682082750922788"
+		// , "865609005136216075"
 	];
 
 	client.on("message", async message => {
 		if(channelList.includes(message.channel.id) && !message.author.bot){
-			message.channel.send("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
-			try{
-				await message.react("👍");
-				await message.react("👎");
-				await message.react("👌");
-			}catch(err) {
-				console.log(err);
-			}
+			message.channel.send("━━━━━━━━━━━━━━━━━━━━━━━━━━");
+
+			await message.react("👍");
+			message.react("👎");
+			// await message.react("👌");
 		}
 	});
 }
