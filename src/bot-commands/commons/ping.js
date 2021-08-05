e82e66f417afa3b60eea878255df06cbc8776ada
saltyfishie98 <randommaker2142@gmail.com>
2021-08-05 22:34:08 +0800

    modified ping

--- src/bot-commands/commons/ping.js
+++ src/bot-commands/commons/ping.js
@@ -3,6 +3,18 @@ module.exports = {
 	shortDesc: "Ping hosting server for respond",
 	maxArgs: 0,
 	callback: (message, args, text) => {
-		message.channel.send("Pong!");
+		message.channel.send("calculating...").then(
+			result => {
+				const ping = result.createdTimestamp - message.createdTimestamp;
+
+				result.edit({
+					content: "",
+					embed:{
+						title: "Pong!",
+						description: `Local latency: ${ping}ms`
+					}
+				});
+			}
+		);
 	}
 };
--- src/slash-commands/fun/s8ball.js
+++ src/slash-commands/fun/s8ball.js
@@ -1,25 +1,4 @@
-const responses = [
-	"It is certain",
-	"It is decidedly so",
-	"Without a doubt",
-	"Yes, definitely",
-	"You may rely on it",
-	"As I see it, yes",
-	"Most likely",
-	"Outlook good",
-	"Yes",
-	"Signs point to yes",
-	"Reply hazy try again",
-	"Ask again later",
-	"Better not tell you now",
-	"Cannot predict now",
-	"Concentrate and ask again",
-	"Don't count on it",
-	"My reply is no",
-	"My sources say no",
-	"Outlook not so good",
-	"Very doubtful "
-];
+const helper = require("../../../webassembly/helper.js");
 
 module.exports = {
 	registration:{
@@ -39,7 +18,7 @@ module.exports = {
 
 	cmdName: "8ball",
 	callback: async (client, interaction, args, replyTo) => {
-		let reply = responses[Math.floor(Math.random() * responses.length)];
+		let reply = helper.response8Ball();
 		replyTo(interaction, `**8ball**: <@${interaction.member.user.id}> ${reply}`);
 	}
 };
